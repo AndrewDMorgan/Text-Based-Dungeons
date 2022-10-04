bff3b6860f2bac7c5ce13c465add987866788dde
Andrew Morgan <75284724+AndrewDMorgan@users.noreply.github.com>
2022-10-04 15:05:36 -0600

    Updated Code
    Updated the code.

--- ExtendedItems.java
+++ ExtendedItems.java
@@ -1,3 +1,6 @@
+import java.util.*;
+import java.io.*;
+
 public class ExtendedItems
 {
     // all the types of items
@@ -8,7 +11,7 @@ public class ExtendedItems
         Ranged,
         Light,
         Defense,
-        Ring
+        Buff
     }
 
     // for all melee weapons
@@ -34,10 +37,27 @@ public class ExtendedItems
 
         // attacks any nearby mobs
         @Override
-        public void Attack(Player player, BaseMob[] mobs)
+        public void Attack(Player player, BaseMob[] mobs, int strength, int dx, int dy)
         {
-            // attacks a mob
+            // looping through all the mobs
+            for (int i = 0; i < mobs.length; i++)
+            {
+                // looping through the length of the weapon
+                for (int r = 1; r <= range; r++)
+                {
+                    // checking if the mobs is on the position being attacked
+                    if (player.GetX() + dx * r == mobs[i].GetX() && player.GetY() + dy * r == mobs[i].GetY())
+                    {
+                        // attacking the mob
+                        mobs[i].Attacked(player, (int)((float)damage * ((float)strength * 0.25)));
+                    }
+                }
+            }
         }
+
+        // prints the basic info
+        @Override
+        public void PrintInfo() {System.out.println(GetName() + "\nDamage: " + damage + "\n");}
 
         // copies all info to a new object
         @Override
@@ -55,10 +75,30 @@ public class ExtendedItems
 
         // attacking the mobs
         @Override
-        public void Attack(Player player, BaseMob[] mobs)
+        public void Attack(Player player, BaseMob[] mobs, int strength, int dx, int dy)
         {
             // attacks a mob
+            // looping through all the mobs
+            for (int i = 0; i < mobs.length; i++)
+            {
+                // looping through the length of the weapon
+                for (int r = 1; r <= GetRange(); r++)
+                {
+                    // checking if the mobs is on the position being attacked
+                    if (player.GetX() + dx * r == mobs[i].GetX() && player.GetY() + dy * r == mobs[i].GetY())
+                    {
+                        // attacking the mob
+                        mobs[i].Attacked(player, (int)((float)GetDamage() * ((float)strength * 0.25)));
+                        break;  // arrows only hit 1 mob at a time
+                    }
+                }
+            }
         }
+
+        // prints the basic info
+        @Override
+        public void PrintInfo() {System.out.println(GetName() + "\nDamage: " + GetDamage() + "\nRange: " + GetRange() + "\n");}
+
         // copies all info to a new object
         @Override
         public Ranged Copy() {return new Ranged(GetName(), GetType(), GetDamage(), GetRange());}
@@ -67,7 +107,7 @@ public class ExtendedItems
     // for all lights
     static public class Light extends Items
     {
-        private int glow;  // the level of glow 0 - 10
+        private int glow;  // the level of glow 0 - 15
 
         // the constructor
         public Light(String name, ExtendedItems.ItemTypes itemType, int glow)
@@ -81,16 +121,21 @@ public class ExtendedItems
         @Override
         public int GetLight() {return glow;}
 
+        // prints the basic info
+        @Override
+        public void PrintInfo() {System.out.println(GetName() + "\nGlow: " + glow + "\n");}
+
         // copies all info to a new object
         @Override
         public Light Copy() {return new Light(GetName(), GetType(), glow);}
     }
 
+    /*  didn't end up being used, was originally planned
     // for all armor/shields/defensive items
     static public class Defense extends Items
     {
         private float defense;  // the defence provided from the item
-        private boolean equipped;  // if the arrmor is active
+        private boolean equipped;  // if the armor is active
 
         // the constructor
         public Defense(String name, ExtendedItems.ItemTypes itemType, float defense)
@@ -104,7 +149,7 @@ public class ExtendedItems
         // equips the armor
         public void Equip() {equipped = true;}
 
-        // unequips the armor
+        // unequipped the armor
         public void UnEquip() {equipped = false;}
 
         // damages the player (if equipped) based on defense
@@ -116,34 +161,91 @@ public class ExtendedItems
             return 0;
         }
 
+        // prints the basic info
+        @Override
+        public void PrintInfo() {System.out.println(GetName() + "\nDefense: " + defense + "\n");}
+
         // copies all info to a new object
         @Override
         public Defense Copy() {return new Defense(GetName(), GetType(), defense);}
     }
+    */
 
-    // an extension of defense for rings
-    static public class Ring extends Defense
+    static public class Buff extends Items
     {
         // all the possible effects from rings
         public enum Effects
         {
             Glow,
             Defense,
-            Regen,
-            Strength
+            Strength,
+            Health
         }
+
+        // The names and types of effects of the buffs
+        private String [] effectNames = {"Glow", "Defense", "Strength", "Health"};
+        private Effects[] effectTypes = {Effects.Glow, Effects.Defense, Effects.Strength, Effects.Health};
+
+        // the player
+        private Player player;
 
         // for the rings affects
         private Effects[] effects;
         private int[] strengths;
 
         // the constructor
-        public Ring(String name, ExtendedItems.ItemTypes itemType, Effects[] effects, int[] strengths)
-        {
-            super(name, itemType, 0);
+        public Buff(String name, ExtendedItems.ItemTypes itemType, Player player, Effects[] effects, int[] strengths) {
+            super(name, itemType);
+
+            this.effects = effects;
+            this.strengths = strengths;
+
+            this.player = player;
         }
 
-        public Ring Copy() {return new Ring(GetName(), GetType(), effects, strengths);}
-    }
+        // activates the buffs powers
+        public void ActivateBuff()
+        {
+            // doesn't get added to the inventory but does give a buff to the player when picked up
+            for (int i = 0; i < this.effects.length; i++)
+            {
+                // checking which buff is currently being checked
+                if      (effects[i] == Effects.Glow    ) player.AddGlow(strengths[i]);
+                else if (effects[i] == Effects.Defense ) player.AddDefense(strengths[i]);
+                else if (effects[i] == Effects.Strength) player.AddStrength(strengths[i]);
+                else if (effects[i] == Effects.Health  ) player.AddHealth(strengths[i]);
+            }
+        }
 
+        // gets the name of an effect
+        private String GetEffectName(Effects effect)
+        {
+            // looping through all the effect types
+            for (int i = 0; i < effectTypes.length; i++)
+            {
+                if (effectTypes[i] == effect) return effectNames[i];  // returning the name
+            }
+            return "No Effect Name Found";  // no name was found
+        }
+
+        // prints the basic info
+        @Override
+        public void PrintInfo()
+        {
+            System.out.println(GetName());  // printing the name of the ring
+
+            // looping through all the effects
+            for (int i = 0; i < effects.length; i++)
+            {
+                // printing the name of the effect
+                System.out.println("Buff: " + GetEffectName(effects[i]) + " x " + strengths[i]);
+            }
+
+            // adding a new line so it renders correctly
+            System.out.println("");
+        }
+
+        @Override
+        public Buff Copy() {return new Buff(GetName(), GetType(), player, effects, strengths);}
+    }
 }
