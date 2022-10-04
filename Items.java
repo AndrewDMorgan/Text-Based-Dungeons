8357e19403703901c45570633d036ebe60e0be26
Andrew Morgan <75284724+AndrewDMorgan@users.noreply.github.com>
2022-10-04 15:06:04 -0600

    Updated the code
    Updated the code.

--- Items.java
+++ Items.java
@@ -10,16 +10,24 @@ public class Items
         this.itemType = itemType;
     }
 
+    // prints the basic info
+    public void PrintInfo() {if (!name.equals("")) System.out.println(name + "\n");}
+
     // gets the name of the item
     public String GetName() {return name;}
 
     public float Attacked() {return 0;}  // does nothing sense this item would have no set defense
 
-    public void Attack(Player player, BaseMob[] mobs) {}  // does nothing sense this item would have no set attack
+    public void Attack(Player player, BaseMob[] mobs, int strength, int dx, int dy) {}  // does nothing sense this item would have no set attack
+
+    public void ActivateBuff() {}  // does nothing sense it isn't a buff
 
     public int GetLight() {return 0;}   // does nothing sense this item would have no set lighting
 
     public ExtendedItems.ItemTypes GetType() {return itemType;}  // gets the items type
+
+    // attacks any nearby mobs
+    public void Attack(Player player, BaseMob[] mobs, int dx, int dy) {};
 
     // copies the item
     public Items Copy() {return new Items(name, itemType);}
