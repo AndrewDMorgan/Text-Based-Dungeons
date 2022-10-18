2c4aed955a6f1a6209b26391bf134c86e0aaceb6
Andrew Morgan <75284724+AndrewDMorgan@users.noreply.github.com>
2022-10-17 18:42:06 -0600

    Added Enchantments
    Added enchantments and updated the capabilities of the item class

--- Items.java
+++ Items.java
@@ -3,18 +3,28 @@ public class Items
     private String name;  // the name of the item
     private ExtendedItems.ItemTypes itemType;  // the type of item
 
+    // the items enchantments
+    private ExtendedItems.Enchants enchants;
+
     // the items constructor
     public Items(String name, ExtendedItems.ItemTypes itemType)
     {
+        // initializing the parameters
         this.name = name;
         this.itemType = itemType;
+
+        // creating the enchantment manager for the item
+        enchants = new ExtendedItems.Enchants(itemType);
     }
 
     // prints the basic info
-    public void PrintInfo() {if (!name.equals("")) System.out.println(name + "\n");}
+    public void PrintInfo() {if (!name.equals("")) System.out.println(name + "\n" + enchants.GetInfo());}
 
     // gets the name of the item
     public String GetName() {return name;}
+
+    // gets the enchantment manager
+    public ExtendedItems.Enchants GetEnchantmentManager() {return enchants;}
 
     public float Attacked() {return 0;}  // does nothing sense this item would have no set defense
 
@@ -22,7 +32,7 @@ public class Items
 
     public void ActivateBuff() {}  // does nothing sense it isn't a buff
 
-    public int GetLight() {return 0;}   // does nothing sense this item would have no set lighting
+    public int GetLight() {return enchants.GetGlow(5);}   // returns whatever light level is assigned from any enchants (0 if no enchantments)
 
     public ExtendedItems.ItemTypes GetType() {return itemType;}  // gets the items type
 
@@ -32,6 +42,4 @@ public class Items
     // copies the item
     public Items Copy() {return new Items(name, itemType);}
 }
-
-
 
