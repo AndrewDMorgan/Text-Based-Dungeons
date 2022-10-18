20fd7d490a6c2cf655c678bf13f10e7ee428a903
Andrew Morgan <75284724+AndrewDMorgan@users.noreply.github.com>
2022-10-17 18:43:27 -0600

    Added Enchantments & A Boss
    Added enchantments, a boss, and finished the second level

--- Map.java
+++ Map.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class Map
 {
     // stores a light (used in the map)
-    static private class Light
+    private static class Light
     {
         public final int x;
         public final int y;
@@ -18,7 +18,7 @@ public class Map
     }
 
     // stores an item and position
-    static public class DroppedItems
+    public static class DroppedItems
     {
         public Items item;
         public int x;
@@ -41,14 +41,17 @@ public class Map
             {
                 new Light(9 , 7 , 6),
                 new Light(9 , 10, 6),
-                new Light(18, 2 , 8)
-            }
+                new Light(18, 2 , 8),
+                new Light(1 , 13, 8),
+                new Light(17, 11, 6)
+            },
+            {}
     };
 
     // the maps (in numbers to make some things easier)
     private final int[][][] levels =
             {
-                {
+                {   // 10 x 10
                     {2, 3, 3, 3, 3, 3, 3, 3, 3, 2},
                     {2, 4, 0, 0, 0, 0, 0, 0, 0, 2},
                     {2, 0, 0, 2, 0, 0, 0, 2, 0, 2},
@@ -60,45 +63,77 @@ public class Map
                     {2, 0, 0, 2, 0, 0, 0, 0, 0, 2},
                     {2, 3, 3, 3, 3, 3, 3, 3, 3, 2}
                 },
-                {
+                {   // 20 x 18
                     {2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 2},
-                    {2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 4, 2},
+                    {2, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 4, 2},
                     {2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 5, 2},
-                    {2, 0, 0, 0, 0, 0, 0, 0, 6, 0, 0, 6, 3, 0, 2, 0, 0, 2, 0, 2},
-                    {2, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 2, 4, 0, 2, 0, 0, 2, 0, 2},
-                    {2, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 2, 0, 0, 2, 0, 0, 2, 0, 2},
-                    {2, 0, 0, 0, 0, 6, 3, 3, 2, 0, 0, 2, 0, 0, 2, 0, 0, 2, 0, 2},
-                    {2, 0, 0, 0, 0, 0, 0, 0, 6, 5, 0, 6, 3, 3, 6, 0, 0, 6, 0, 2},
+                    {2, 0, 0, 6, 3, 3, 3, 3, 6, 0, 0, 6, 3, 0, 2, 0, 0, 2, 0, 2},
+                    {2, 0, 0, 2, 0, 0, 0, 0, 2, 0, 0, 2, 4, 0, 2, 0, 0, 2, 0, 2},
+                    {2, 0, 0, 2, 0, 2, 7, 0, 2, 0, 0, 2, 0, 0, 2, 0, 0, 2, 0, 2},
+                    {2, 0, 0, 2, 0, 2, 4, 0, 2, 0, 0, 2, 0, 0, 2, 0, 0, 2, 0, 2},
+                    {2, 0, 0, 2, 0, 3, 3, 3, 6, 5, 0, 6, 3, 3, 6, 0, 0, 2, 0, 2},
                     {2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
                     {2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
-                    {2, 0, 0, 0, 0, 6, 3, 3, 6, 5, 0, 6, 3, 3, 6, 0, 0, 6, 3, 2},
-                    {2, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 2},
-                    {2, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 2},
-                    {2, 0, 0, 0, 0, 0, 0, 0, 6, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 2},
-                    {2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
-                    {2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
-                    {2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+                    {2, 3, 3, 3, 3, 3, 3, 3, 6, 5, 0, 6, 3, 3, 6, 0, 6, 3, 3, 2},
+                    {2, 7, 0, 0, 4, 2, 0, 4, 2, 0, 0, 2, 4, 0, 2, 0, 2, 5, 4, 2},
+                    {2, 0, 0, 0, 0, 2, 0, 0, 2, 0, 0, 2, 0, 0, 0, 0, 2, 0, 0, 2},
+                    {2, 5, 0, 0, 0, 2, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 2, 0, 0, 2},
+                    {2, 0, 3, 3, 3, 3, 3, 3, 3, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 2},
+                    {2, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 2, 0, 2, 0, 2, 0, 0, 2},
+                    {2, 0, 0, 2, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 2},
                     {2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 2}
+                },
+                {   // 10 x 10
+                    {2, 3, 3, 3, 3, 3, 3, 3, 3, 2},
+                    {2, 0, 0, 0, 0, 0, 0, 0, 4, 2},
+                    {2, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+                    {2, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+                    {2, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+                    {2, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+                    {2, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+                    {2, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+                    {2, 4, 0, 0, 0, 0, 0, 0, 4, 2},
+                    {2, 3, 3, 3, 3, 3, 3, 3, 3, 2}
                 }
             };
 
-    // 0 🧟: zombie, 1 ఠఠ: slime, 2 ☠): skeleton
-    private int[][] mobLoot = {
-            {0, 0, 0, 1, 1, 2, 10, 12},
-            {0, 0, 0, 0, 0, 2, 2, 4, 4, 7, 3, 3, 10, 11, 11},
-            {0, 0, 0, 5, 5, 2, 10, 13}
+    // 0 🧟: zombie, 1 ఠఠ: slime, 2 ☠): skeleton, 3; boss loot
+    private final int[][] mobLoot = {
+            {0, 0, 0, 1, 2},
+            {0, 0, 0, 0, 11},
+            {0, 0, 0, 2, 5},
+            {12, 13, 14, 15}
     };
 
     // the mobs in each level
     private BaseMob[][] mobs = {
             {
-                new BaseMob(5, 3, 1, 1, 1, "\uD83E\uDDDF", mobLoot[0]),
+                /*new BaseMob(5, 3, 1, 1, 1, "\uD83E\uDDDF", mobLoot[0]),
                 new BaseMob(8, 2, 1, 1, 1, "ఠఠ"          , mobLoot[1]),
                 new BaseMob(6, 8, 3, 1, 1, "\uD83E\uDDDF", mobLoot[0]),
-                new BaseMob(6, 5, 2, 1, 1, "☠)"          , mobLoot[2])
+                new BaseMob(6, 5, 2, 1, 1, "☠)"          , mobLoot[2])*/
             },
             {
-                new BaseMob(5, 3, 1, 1, 1, "\uD83E\uDDDF", mobLoot[0])
+                /*new BaseMob(5 , 3 , 1, 1, 1, "\uD83E\uDDDF", mobLoot[0]),
+                new BaseMob(7 , 12, 1, 1, 1, "☠)"          , mobLoot[2]),
+                new BaseMob(7 , 16, 1, 1, 1, "ఠఠ"          , mobLoot[1]),
+                new BaseMob(3 , 15, 2, 2, 1, "\uD83E\uDDDF", mobLoot[0]),
+                new BaseMob(2 , 16, 2, 1, 1, "☠)"          , mobLoot[2]),
+                new BaseMob(1 , 14, 3, 2, 1, "ఠఠ"          , mobLoot[1]),
+                new BaseMob(3 , 13, 2, 2, 1, "\uD83E\uDDDF", mobLoot[0]),
+                new BaseMob(2 , 11, 3, 1, 1, "\uD83E\uDDDF", mobLoot[0]),
+                new BaseMob(16, 16, 1, 1, 1, "ఠఠ"          , mobLoot[1]),
+                new BaseMob(18, 12, 2, 1, 1, "\uD83E\uDDDF", mobLoot[0]),
+                new BaseMob(18, 7 , 1, 1, 1, "☠)"          , mobLoot[2]),
+                new BaseMob(12, 5 , 2, 2, 1, "ఠఠ"          , mobLoot[1]),
+                new BaseMob(10, 1 , 1, 1, 1, "\uD83E\uDDDF", mobLoot[0]),
+                new BaseMob(2 , 5 , 2, 1, 1, "ఠఠ"          , mobLoot[1]),
+                new BaseMob(7 , 5 , 3, 2, 1, "ఠఠ"          , mobLoot[1]),
+                new BaseMob(5 , 4 , 5, 3, 2, "☠)"          , mobLoot[2]),
+                new BaseMob(4 , 4 , 2, 1, 1, "\uD83E\uDDDF", mobLoot[0])*/
+            },
+            {
+                new Boss(5, 5, 15, 1, 2, "\uD83E\uDDDF", mobLoot[3])
             }
     };
 
@@ -106,29 +141,34 @@ public class Map
 
     // the loots for chests in different levels (each level has an array of valid ids for loot)
     // 0; nothing, 1; wooden sword, 2; ancient torch, 3; ancient axe, 4; wooden spear, 5; wooden bow, 6; stone sword, 7; tiki torch, 8; stone spear, 9; Rusty Sword, 10; cross bow
-    // buffs 11; health buff, 12; glow buff, 13; strength buff, 14; defense buff
+    // 11; health buff, 12; glow buff, 13; strength buff, 14; defense buff, 15; defender buff (strength + defense)
+    // legendary/defender items - 16; sword, 17; bow, 18; spear, 19; torch
     private final int[][] levelChestLoot = {
-            {1, 1, 1, 1, 2, 2, 2, 2, 4, 4, 4, 5, 5, 5, 6, 6, 10, 11, 12, 12, 12, 13, 14},
-            {2, 3, 4, 4, 5, 5, 6, 6, 7, 8, 10, 10, 11, 12, 12, 13, 14}
+            {1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 4, 4, 4, 4, 5, 5, 5, 11, 11, 11, 14},
+            {2, 2, 2, 2, 3, 3, 3, 3, 5, 5, 5, 5, 6, 6, 7, 7, 8, 8, 10, 10, 11, 11, 11, 11, 12, 13, 14, 15},
+            {11, 11, 11, 11, 11, 11, 12, 13, 14}
     };
 
     // the different tiles light goes through
-    private final int[] transparent = {0, 4, 5};
+    private final int[] transparent = {0, 4, 5, 7};
 
     // the solid tiles
-    public final int[] collisionTiles = {1, 2, 3, 4, 6};
+    public final int[] collisionTiles = {1, 2, 3, 4, 6, 7};
+
+    // the tile that represents enchantment tables
+    public final int enchantmentTableTile = 7;
 
     // the level spawn points
-    private final int[] levelSpawnsX = {4, 10};
-    private final int[] levelSpawnsY = {5, 9};
+    private final int[] levelSpawnsX = {4, 10, 1};
+    private final int[] levelSpawnsY = {5, 9, 1};
 
     // the current level
     private int level = 0;
 
-    // 0; air, 1; water, 2; wall, 3; bottom wall, 4; chest, 5; torch facing ->, 6; corner wall
-    private final String[] chars = {"  ", "~~", "||", "--", "==", "; ", "++"};
+    // 0; air, 1; water, 2; wall, 3; bottom wall, 4; chest, 5; torch facing ->, 6; corner wall, 7; enchantment table
+    private final String[] chars = {"  ", "~~", "||", "--", "==", "; ", "++", "\uD83D\uDCD6"};
 
-    // the different brightnesses (9 is full bright 0 is completely dark) (@%#*+=-:. )
+    // the different brightnesses (14 or 15 is full bright 0 is completely dark) (@%#*+=-:. )
     //private final String[] charBrightness = {"##", "**", "::", ". ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "};
     private final String[] charBrightness = {"██", "▓▓", "▒▒", "░░", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "};
 
@@ -137,7 +177,18 @@ public class Map
     // the initializer (nothing currently)
     public Map()
     {
+        // the size of the first level
+        int lx = levels[level]   .length;
+        int ly = levels[level][0].length;
 
+        // initializing the brightness map
+        levelBrightnessMap = new int[lx][ly];
+
+        // setting up the brightness map
+        for (int x = 0; x < lx; x++)
+        {
+            for (int y = 0; y < ly; y++) levelBrightnessMap[x][y] = 0;
+        }
     }
 
     private void GetMap(Player player)
@@ -159,7 +210,7 @@ public class Map
                 if (tile.equals("  ") || brightness <= 1)
                 {
                     // altering based on brightness
-                    tile = charBrightness[brightness];
+                    tile = charBrightness[Math.min(brightness, 15)];
                 }
 
                 // checking if rendering the player
@@ -247,12 +298,6 @@ public class Map
         int lx = levels[level]   .length;
         int ly = levels[level][0].length;
 
-        levelBrightnessMap = new int[lx][ly];  // creating a new light map
-
-        for (int x = 0; x < lx; x++)
-        {
-            for (int y = 0; y < ly; y++) levelBrightnessMap[x][y] = 0;
-        }
         // adding all the lights
         for (int i = 0; i < lights; i++)
         {
@@ -289,7 +334,7 @@ public class Map
         Light[] levelLights = mapLights[level];  // all the lights on this level
 
         // generating the lighting from player items and map lights
-        int lights = levelLights.length+1;
+        int lights = levelLights.length + 1;
         int[] lightsX = new int[lights];
         int[] lightsY = new int[lights];
         int[] lightsStrength = new int[lights];
@@ -335,7 +380,8 @@ public class Map
         {
             // choosing a random item
             int index = (int)Math.round(Math.random() * (double)(levelChestLoot[level].length-1));
-            items[i] = player.IDGetItem(levelChestLoot[level][index]).Copy();  // adding the item to the final items (a new instance of it)
+            Items item = player.IDGetItem(levelChestLoot[level][index]).Copy();
+            items[i] = item;  // adding the item to the final items (a new instance of it)
         }
         return items;
     }
@@ -346,12 +392,29 @@ public class Map
         int numLevels = levels.length;  // the number of levels
         level++;  // going to the next level
 
+        // removing all dropped items
+        droppedItems = new ArrayList<DroppedItems>();
+
         if (level >= numLevels)
         {
             System.out.println("You Finished The Game!!!\nGreat Job!!");  // the player has beat all levels
             level--;  // going back a level to stop any crashes
             return true;  // the game was beaten
         }
+
+        // the size of the new map
+        int lx = levels[level]   .length;
+        int ly = levels[level][0].length;
+
+        // creating a new map with the correct size
+        levelBrightnessMap = new int[lx][ly];
+
+        // resetting the brightness map for the new level
+        for (int x = 0; x < lx; x++)
+        {
+            for (int y = 0; y < ly; y++) levelBrightnessMap[x][y] = 0;
+        }
+
         return false;  // nothing happened
     }
 
@@ -370,7 +433,13 @@ public class Map
     // gets the brightness at the given point
     public int GetBrightness(int x, int y) {return levelBrightnessMap[y][x];}
 
+    // checks if the position is on an enchantment table
+    public boolean CheckEnchantmentTable(int x, int y) {return levels[level][y][x] == enchantmentTableTile;}
+
     // gets the starting x and y for a map
     public int GetStartX() {return levelSpawnsX[level];}
     public int GetStartY() {return levelSpawnsY[level];}
+
+    // removes a tile from the map
+    public void RemoveTile(int x, int y) {levels[level][y][x] = 0;}
 }
